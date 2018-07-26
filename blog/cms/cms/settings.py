cb3038321450b13b70d2e7e7521513801c735da2
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2018-07-26 14:10:41 +0600

    changed bootstrap design

--- blog/cms/cms/settings.py
+++ blog/cms/cms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'blog',
 ]
 
@@ -122,4 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-print(STATICFILES_DIRS)
+
+CRISPY_TEMPLATE_PACK  = "bootstrap3"
